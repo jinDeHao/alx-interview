7fc77c804cf112c56bcc2abc491f37a5d02dcbc8
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-05 00:32:01 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
             continue
         row = [1]
         for j in range(1, i):
-            row.append(pascal[i-1][j - 1] + pascal[i-1][j])
+            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
         row.append(1)
         pascal.append(row)
     return pascal
