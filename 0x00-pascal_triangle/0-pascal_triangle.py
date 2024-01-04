6e64874c7f52f40d030bd2d43c5b6428cd756785
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-05 00:30:48 +0100

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def pascal_triangle(n):
+    pascal = []
+    if n <= 0:
+        return pascal
+    for i in range(n):
+        if i == 0:
+            pascal.append([1])
+            continue
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal[i-1][j - 1] + pascal[i-1][j])
+        row.append(1)
+        pascal.append(row)
+    return pascal
