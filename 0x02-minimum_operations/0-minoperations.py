5086b8c17fe9036b79d111161669053f69d38fd4
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-21 19:20:42 +0100

    copy all past

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,11 +13,15 @@ def minOperations(n: int) -> int:
     if n <= 1:
         return 0
     cnoc = 1
-    opNum = 1
+    """copy all"""
     clipboard_size = 1
-    while cnoc <= n:
+    opNum = 1
+    while cnoc < n:
         if cnoc != 1 and n % cnoc == 0:
-            clipboard_size = cnoc / clipboard_size
+            """copy all"""
+            clipboard_size = cnoc
+            opNum += 1
+        """past"""
         cnoc += clipboard_size
         opNum += 1
     return (opNum)
