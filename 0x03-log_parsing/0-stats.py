277ff4e22f897c39c928116f59d9321bba407998
jinDeHao <o.idhmaid@gmail.com>
2024-01-26 00:48:15 +0100

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,6 +8,7 @@
 import signal
 import sys
 import re
+import time
 
 regex = '^([0-9]?[0-9]?[0-9]\.){3}[0-9]?[0-9]?[0-9] \
 - \[\S* \S*] "\S* \S* \S*" [2-5]0[0,1,3,4,5] [0-9]+$'
@@ -28,24 +29,31 @@ filesize = 0
 lineindex = 0
 
 
-def SignalHandler_SIGINT(signum, frame):
+def SignalHandler_SIGINT():
     """Ctrl C Signal Handler"""
     print("File size: {}".format(filesize))
     for code, num in valid_status_code.items():
         if num:
             print("{}: {}".format(code, num))
 
-signal.signal(signal.SIGINT, SignalHandler_SIGINT)
 
-for line in sys.stdin:
-    lineindex += 1
-    if re.match(regex, line):
-        filesize += int(re.search(filesizeregex, line).group(0))
-        statuscode = re.search(statuscoderegex, line).group(0)[2:]
-        if statuscode in valid_status_code:
-            valid_status_code[statuscode] += 1
-    if lineindex % 10 == 0:
-        print("File size: {}".format(filesize))
-        for code, num in valid_status_code.items():
-            if num:
-                print("{}: {}".format(code, num))
+# signal.signal(signal.SIGINT, SignalHandler_SIGINT)
+
+try:
+    for line in sys.stdin:
+        lineindex += 1
+        try:
+            if re.match(regex, line):
+                filesize += int(re.search(filesizeregex, line).group(0))
+                statuscode = re.search(statuscoderegex, line).group(0)[2:]
+                if statuscode in valid_status_code:
+                    valid_status_code[statuscode] += 1
+        except Exception:
+            pass
+        if lineindex % 10 == 0:
+            print("File size: {}".format(filesize))
+            for code, num in valid_status_code.items():
+                if num:
+                    print("{}: {}".format(code, num))
+finally:
+    SignalHandler_SIGINT()
