04a1a024422c6a82b27a0ae643225a8d0b7a9428
jinDeHao <o.idhmaid@gmail.com>
2024-02-07 23:06:22 +0100

    valid

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,6 +10,24 @@ def validUTF8(data):
     for b in data:
         try:
             bytes([b]).decode('utf-8')
-            return True
         except UnicodeDecodeError:
             return False
+    return True
+
+# def validUTF8(data):
+#     expected_continuations = 0
+#     for byte in data:
+#         if expected_continuations == 0:
+#             if (byte >> 5) == 0b110:
+#                 expected_continuations = 1
+#             elif (byte >> 4) == 0b1110:
+#                 expected_continuations = 2
+#             elif (byte >> 3) == 0b11110:
+#                 expected_continuations = 3
+#             elif (byte >> 7):
+#                 return False
+#         else:
+#             if (byte >> 6) != 0b10:
+#                 return False
+#             expected_continuations -= 1
+#     return expected_continuations == 0
