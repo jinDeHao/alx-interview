a022181058dbd1fb7433df6e13232ccde45c9b4f
jinDeHao <o.idhmaid@gmail.com>
2024-02-15 23:59:31 +0100

    req

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,7 +10,8 @@ const fun = async () => {
     return;
   }
   try {
-    let req = await request( `https://swapi-api.alx-tools.com/api/films/${num[0]}`);
+    const url = `https://swapi-api.alx-tools.com/api/films/${num[0]}`
+    let req = await request(url);
     const characters = await JSON.parse(req.body).characters;
     if (characters === undefined) {
       return;
