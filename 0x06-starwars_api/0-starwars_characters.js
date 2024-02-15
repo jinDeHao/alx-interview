62e26ef54831fe8942f64f3b3be274564279e91f
jinDeHao <o.idhmaid@gmail.com>
2024-02-16 00:20:17 +0100

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,21 @@
 #!/usr/bin/node
 
-
 const util = require('util');
 const request = util.promisify(require('request'));
 
 const fun = async () => {
   const num = process.argv.slice(2);
-  if (num.length === 0) {
-    return;
-  }
+  if (num.length === 0) return;
   try {
     let req = await request(`https://swapi-api.alx-tools.com/api/films/${num[0]}`);
     const characters = await JSON.parse(req.body).characters;
-    if (characters === undefined) {
-      return;
-    }
-    for (let i = 0; i < characters.length; i++) {
-      req = await request(characters[i]);
+    if (characters === undefined) return;
+    for (const char of characters) {
+      req = await request(char);
       const character = await JSON.parse(req.body);
       console.log(character.name);
     }
-  } catch (e) {
-    return e;
-  }
-}
+  } catch (e) { return e; }
+};
 
 fun();
