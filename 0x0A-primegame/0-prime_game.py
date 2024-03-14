dc6b5cd349e924a9ccc77aaa148592964cab5506
jinDeHao <o.idhmaid@gmail.com>
2024-03-14 01:48:49 +0000

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,55 +20,56 @@
 #     return count
 
 
-# def isWinner(x, nums):
-#     """
-#     Maria and Ben are playing a game.
-#     Given a set of consecutive integers starting
-#     from 1 up to and including n, they take turns
-#     choosing a prime number from the set and removing that
-#     number and its multiples from the set.
-#     The player that cannot make a move loses the game.
-#     """
-#     if x == 0 or len(nums) == 0:
-#         return None
-#     players = {"Maria": 0, "Ben": 0}
-#     for num in nums[:x]:
-#         count_p = count_prime([i for i in range(1, num + 1)])
-#         if count_p % 2 == 0:
-#             players['Ben'] += 1
-#         else:
-#             players['Maria'] += 1
-#     if players['Ben'] > players['Maria']:
-#         return 'Ben'
-#     if players['Maria'] > players['Ben']:
-#         return 'Maria'
-#     return None
-
-def get_primes(n):
-    """List of primes up to n."""
-    primes = []
+def count_primes(n):
+    """check if one item in list is prime"""
+    number = 0
     sieve = [True] * (n + 1)
     for p in range(2, n + 1):
         if sieve[p]:
-            primes.append(p)
+            number += 1
             for i in range(p, n + 1, p):
                 sieve[i] = False
-    return primes
+    return number
 
 
 def isWinner(x, nums):
-    """ Determine the winner of the game. """
-    if not x or not nums:
+    """
+    Maria and Ben are playing a game.
+    Given a set of consecutive integers starting
+    from 1 up to and including n, they take turns
+    choosing a prime number from the set and removing that
+    number and its multiples from the set.
+    The player that cannot make a move loses the game.
+    """
+    if x == 0 or len(nums) == 0:
         return None
-    maria_count = ben_count = 0
+    players = {"Maria": 0, "Ben": 0}
     for num in nums[:x]:
-        primes = get_primes(num)
-        if len(primes) % 2 == 0:
-            ben_count += 1
+        count_p = count_primes(num)
+        if count_p % 2 == 0:
+            players['Ben'] += 1
         else:
-            maria_count += 1
-    if maria_count > ben_count:
-        return 'Maria'
-    elif ben_count > maria_count:
+            players['Maria'] += 1
+    if players['Ben'] > players['Maria']:
         return 'Ben'
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
     return None
+
+
+# def isWinner(x, nums):
+#     """ Determine the winner of the game. """
+#     if not x or not nums:
+#         return None
+#     maria_count = ben_count = 0
+#     for num in nums[:x]:
+#         primes = get_primes(num)
+#         if len(primes) % 2 == 0:
+#             ben_count += 1
+#         else:
+#             maria_count += 1
+#     if maria_count > ben_count:
+#         return 'Maria'
+#     elif ben_count > maria_count:
+#         return 'Ben'
+#     return None
